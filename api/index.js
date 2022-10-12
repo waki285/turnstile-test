1a80ba0dc0f5792f8460537bce60d60ca961cee7
すずねーう <suzuneu@suzuneu.com>
2022-10-12 06:00:52 +0000

    a

---
+++ api/index.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const axios = require("axios");
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public/index.html"))
+});
+
+app.post("/", async (req, res) => {
+  const response = await axios.post(
+    "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+    `secret=${process.env.SECRET}&response=${req.body["cf-turnstile-response"]}`,
+    {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    }
+  );
+  res.json(response.data);
+});
+
+app.listen(process.env.PORT || 8080);
