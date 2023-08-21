8d5b180c0054da2e6f8206f53cc6df376fedf983
すずねーう <67305123+waki285@users.noreply.github.com>
2023-08-21 13:18:57 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -13,9 +13,10 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", async (req, res) => {
+  const secret = req.body.type ? process.env[`${req.body.type.toUpperCase()}_SECRET`]:process.env.MANAGED_SECRET;
   const response = await axios.post(
     "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-    `secret=${process.env.SECRET}&response=${req.body["cf-turnstile-response"]}`,
+    `secret=${secret}&response=${req.body["cf-turnstile-response"]}`,
     {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded'
